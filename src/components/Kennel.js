ec8f81017fe68e75133a1e85ffd1d374e854ea0a
Hunter Metts <huntermettsdev@gmail.com>
2019-01-17 11:16:47 -0600

    first react app git remote add origin https://github.com/huntermetts/first_react_app.git git push -u origin master

---
+++ src/components/Kennel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import EmployeeList from "./employee/EmployeeList"
+import LocationList from './location/LocationList';
+  // Import EmployeeList component
+
+
+export default class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Student Kennels</h3>
+                <h4>Nashville North Location</h4>
+                <h5>500 Puppy Way</h5>
+                <EmployeeList />
+                <LocationList />
+            </div>
+        );
+    }
+}
---
+++ src/components/location/LocationList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
+    render() {
+        return (
+           <section>
+               <h1>Nashville North: <br></br></h1>
+               1443 BlueBell Way
+                <h1>Nashville South: <br></br></h1>
+                3441 RedBell Drive
+            </section>
+        );
+    }
+}
